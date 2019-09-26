c126ee51a4366695636986656b7de89281eb4e09
clairecoiraton <54407865+clairecoiraton@users.noreply.github.com>
2019-09-26 09:43:22 +0200

    Update readme_Mother&EmbryoEDGE.m

--- readme_Mother&EmbryoEDGE.m
+++ readme_Mother&EmbryoEDGE.m
@@ -1,6 +1,7 @@
 % Matlab code used for comparing the elemental signatures deposited at the
 % vertebral edge between each pregnant female and her embryos.
 %By C. Coiraton May 2017
+
 %All data manipulations and multivariate statistical analyses of were 
 %performed using the free download Fathom Toolbox for MatlabTM (Jones DL, 2017)
 %Fathom Toolbox for Matlab: software for multivariate ecological and oceanographic data analysis.
@@ -8,30 +9,6 @@
 %http://www.marine.usf.edu/user/djones/matlab/matlab.html 
 
 
-% List of embryo data from transect.xls:
-% 
-% date	         loc	num	vert	PT	slide  Embryo
-% '29-Mar-2017'	PM	  15	   1	   46	7		 4
-% '29-Mar-2017'	PM	  15	   2	   47	7		 2
-% '29-Mar-2017'	PM	  15	   3		48	7		 3
-% '29-Mar-2017'	PM	  6	   4		49	7		 1
-% '29-Mar-2017'	PM	  15	   5		50	7		 1
-% '29-Mar-2017'	PM	  6	   6		51	7		 2
-% '29-Mar-2017'	PM	  9	   7		52	7		 2
-% '29-Mar-2017'	PM	  10	   8		53	7		 1
-% '29-Mar-2017'	PM	  6	   9		54	7		 4
-% '29-Mar-2017'	PM	  6	   10		55	7		 3
-% '29-Mar-2017'	PM	  9	   11		56	7		 3
-% '29-Mar-2017'	PM	  10	   12		57	7		 2
-% '29-Mar-2017'	PM	  9	   13		58	7		 1
-% 13-May-2017'	PM	  10	   5		59	18		 2
-% 13-May-2017'	 PM	  6	   6		60	18		 4
-% 
-% - there are n = 15 samples
-% - includes CO, ED, and PT
-% 
-% 170329: no FO to match the ED sample for spot 580, must've been broken?
-% 170329: no ED to match the FO sample for spot 585, must've been broken? 
 
 % Load data:
 load all_HH.mat HH spots;
@@ -64,7 +41,7 @@ saver;
 % 
 % spots = structure with the following fields:
 %   .spot   = unique spot scan identifier (average of 2-3 replicates)
-%   .type   = type of spot sample (CO, PB, BM, GB, ED)
+%   .type   = type of spot sample (FO, BM, GB, ED)
 %   .loc    = location of collection site
 %   .num    = specimen number from this site
 %   .yr     = collection year
@@ -77,37 +54,9 @@ saver;
 %   .gDate  = date of LA-ICP-MS run
 % 
 %   .loc_Rx  = location code
-%   .type_Rx = (CO=1, PB=2, BM=3, GB=4, ED=5)
+%   .type_Rx = (FO=1, PB=2, BM=3, GB=4, ED=5)
 %   .GB_Rx   = growth band code (0-14)
 %   .ED_Rx   = edge code (0-1)
-
-% Elements:
-% 1   'Li7'
-% 2   'Na23'
-% 3   'Mg24'
-% 4   'P31'
-% 5   'Ca43'
-% 6   'Sc45'
-% 7   'V51'
-% 8   'Cr53'
-% 9   'Mn55'
-% 10  'Fe57'
-% 11  'Co59'
-% 12  'Ni60'
-% 13  'Cu63'
-% 14  'Zn64'
-% 15  'Cu65'
-% 16  'Ge72'
-% 17  'Rb85'
-% 18  'Sr88'
-% 19  'Y89'
-% 20  'Cd114'
-% 21  'Sn118'
-% 22  'Ba137'
-% 23  'Au197'
-% 24  'Pb208'
-% 25  'Th232'
-% 26  'U238'
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -133,7 +82,7 @@ pLOD'
 
 % Get index to target elements (= not consistently below LOD):
 tar = find(pLOD<0.1);
-tar(ismember(tar,[5 15])) = []; % remove Ca43 & Cu65
+tar(ismember(tar,[5])) = []; % remove Ca43 
 HH.txt_iso(tar)'                % show target elements
 
 % Confirm target elements are above LOD (should be all 0's):
@@ -167,7 +116,7 @@ age     = age(key);  % sort age
 
 
 % CAP:
-f_capOptimal(Y,'euc',grp,1,1);
+f_capOptimal(Y,'euc',grp,1,1);%spatial median
 
 cap = f_cap(Y,'euc',grp,[],1,1000,1,12,1); % <- provide optimal m=12
 
@@ -213,8 +162,6 @@ rf_GEO = f_RFclass(Y,grp,[],[],0,1,'stnd',1);
 
 % Test the significance of the observed classification success rate:
 f_chanceClass(grp,0.7636,1000,1);
-f_chanceClass(grp,0.7538,1000,1);
-
 
 %  Create a Random Forest Canonical Discriminant Analysis plot:
 f_RFvis(rf_GEO,0.95,1000,0.05);
