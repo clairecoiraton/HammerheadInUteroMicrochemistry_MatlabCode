35c2925435fa5c40d570ccdd6c0b554d1cdacaeb
clairecoiraton <54407865+clairecoiraton@users.noreply.github.com>
2019-09-26 09:41:34 +0200

    Update readme_ExampleYOYSrBaPT.m

--- readme_ExampleYOYSrBaPT.m
+++ readme_ExampleYOYSrBaPT.m
@@ -1,6 +1,7 @@
 % Example for finding peaks or valleys in the in utero profile transect data of
 % the young-of-the-year captured in Salina Cruz
 % By C. Coiraton May 2017
+
 % All data manipulations and multivariate statistical analyses of were 
 % performed using the free download Fathom Toolbox for MatlabTM (Jones DL, 2017)
 % Fathom Toolbox for Matlab: software for multivariate ecological and oceanographic data analysis.
@@ -21,9 +22,7 @@ load ./2017-05-14/170514.mat tra_91;
 X = tra_91;
 
 % Find valleys in Sr:Ba signals:
-[pks,loc] = f_peaks_PT(X,{'Sr88' 'Ba137'},1,0,50,1,10,10);
-
-
+[pks,loc] = f_peaks_PT(X,{'Sr88' 'Ba137'},1,0,11,1,3,2);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %TRANSECT 92 - SC-LEW-19
@@ -34,10 +33,7 @@ load ./2017-05-14/170514.mat tra_92;
 X = tra_92;
 
 % Find valleys in Sr:Ba signals:
-[pks,loc] = f_peaks_PT(X,{'Sr88' 'Ba137'},1,0,50,1,10,10);
-
-
-
+[pks,loc] = f_peaks_PT(X,{'Sr88' 'Ba137'},1,0,11,1,3,2);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %TRANSECT 93 - SC-LEW-45
@@ -48,6 +44,6 @@ load ./2017-05-14/170514.mat tra_93;
 X = tra_93;
 
 % Find valleys in Sr:Ba signals:
-[pks,loc] = f_peaks_PT(X,{'Sr88' 'Ba137'},1,0,50,1,10,10);
+[pks,loc] = f_peaks_PT(X,{'Sr88' 'Ba137'},1,0,11,1,3,2);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
